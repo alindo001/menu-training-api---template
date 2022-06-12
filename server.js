71594701c21f1aebd6385202d4910fb43bdda0b5
Andres <andreslindo20@gmail.com>
2022-06-12 02:10:25 -0400

    api update

--- server.js
+++ server.js
@@ -44,62 +44,25 @@ const menuItems = [ {
     "price": "$24.00",
     "img": "https://media-cdn.tripadvisor.com/media/photo-m/1280/18/cd/03/93/pork-belly-noodle-bowl.jpg",
     "ingredients": "Noodles, Pork Belly, Broth",
-    "nickname": "Heisenberg",
-    "appearance": [
-        1,
-        2,
-        3,
-        4,
-        5
-    ],
-    "portrayed": "Bryan Cranston",
-    "category": "Breaking Bad",
-    "better_call_saul_appearance": []
+    "alergens": "gluten, eggs, soy",
 },
 {
-    "char_id": 1,
-    "name": "Walter White",
-    "birthday": "09-07-1958",
-    "occupation": [
-        "High School Chemistry Teacher",
-        "Meth King Pin"
-    ],
-    "img": "https://images.amcnetworks.com/amc.com/wp-content/uploads/2015/04/cast_bb_700x1000_walter-white-lg.jpg",
-    "status": "Presumed dead",
-    "nickname": "Noodle, Pork Belly, Broth",
-    "appearance": [
-        1,
-        2,
-        3,
-        4,
-        5
-    ],
-    "portrayed": "Bryan Cranston",
-    "category": "Breaking Bad",
-    "better_call_saul_appearance": []
+    "char_id": 2,
+    "itemName": "Asian-marinated Airline Chicken",
+    "price": "$24.00",
+    "img": "https://media.wdwnt.com/2019/10/new-kona-cafe-menu-sticky-wings-airplane-chicken-polynesian-resort_17-1200x675.jpg",
+    "ingredients": "Sticky Rice, Bok Choy, Asian Glaze",
+    "alergens": "dairy, soy",
 },
 {
-    "char_id": 1,
-    "name": "Walter White",
-    "birthday": "09-07-1958",
-    "occupation": [
-        "High School Chemistry Teacher",
-        "Meth King Pin"
-    ],
-    "img": "https://images.amcnetworks.com/amc.com/wp-content/uploads/2015/04/cast_bb_700x1000_walter-white-lg.jpg",
-    "status": "Presumed dead",
-    "nickname": "Heisenberg",
-    "appearance": [
-        1,
-        2,
-        3,
-        4,
-        5
-    ],
-    "portrayed": "Bryan Cranston",
-    "category": "Breaking Bad",
-    "better_call_saul_appearance": []
+    "char_id": 3,
+    "itemName": "Turkey Banh Mi",
+    "price": "$18.00",
+    "img": "https://i1.wp.com/www.wdwopinion.com/wp-content/uploads/2020/01/IMG_8386-scaled.jpg?fit=1024%2C768&ssl=1",
+    "ingredients": "Slow-roasted Turkey Breast, Black Pepper Bacon, Pork Pâte, Cilantro, Jalapeño, Onions, and Grilled Poblano Mayonnaise served with French Fries",
+    "alergens": "gluten, dairy",
 },
+
 
 ]
 
